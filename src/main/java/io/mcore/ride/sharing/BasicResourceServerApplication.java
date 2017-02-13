8b74afe858233eb43edfd0b718815419132193d9
Mario Cuellar <mariocuellar1@gmail.com>
2017-02-13 15:59:40 +0100

    Primera versión: Base Resource Server

---
+++ src/main/java/io/mcore/ride/sharing/BasicResourceServerApplication.java
@@ -0,0 +1,61 @@
+package io.mcore.ride.sharing;
+
+import java.security.Principal;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+public class BasicResourceServerApplication extends ResourceServerConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicResourceServerApplication.class, args);
+	}
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+	@RequestMapping("/data")
+    public String readFoo() {
+        return "read foo " + UUID.randomUUID().toString();
+    }
+	
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                //.antMatchers(HttpMethod.GET, "/user").hasAuthority("ADMIN")
+                .antMatchers("/**").authenticated();
+                //.antMatchers(HttpMethod.POST, "/foo").hasAuthority("FOO_WRITE");
+                //you can implement it like this, but I show method invocation security on write
+    }
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("foo").tokenStore(tokenStore);
+    }
+
+    @Autowired
+    TokenStore tokenStore;
+
+    @Autowired
+    JwtAccessTokenConverter tokenConverter;
+
+}
